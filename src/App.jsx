323cfab3c2836f735a70a961fb1c8ac60d357b4e
Ramom Ferraz <ramomferraz@hotmail.com>
2023-01-02 02:42:36 -0300

    Importação de layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import style from "./App.module.css"
 import Home from './Pages/home'
+import Layout from "./components/Layout"
 
 function App() {
   return (
     <div className={style.app}>
+      <Layout />
       <Home />
     </div>
   )
