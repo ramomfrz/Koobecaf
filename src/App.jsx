6011c417b7c499ce8ecb2c808e47475080b7aabf
Ramom Ferraz <ramomferraz@hotmail.com>
2023-01-02 02:36:34 -0300

    Estrutura react e criação da Home

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import style from "./App.module.css"
+import Home from './Pages/home'
+
+function App() {
+  return (
+    <div className={style.app}>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/home/index.jsx
@@ -0,0 +1,11 @@
+import style from './home.module.css';
+
+function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
